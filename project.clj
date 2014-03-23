e5becbd537e08172b676b2ac11bbfdc90c304484
jason <wilsonjm@g.cofc.edu>
2014-03-23 13:25:17 -0400

    update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lighttable/csharp "0.0.1"
+(defproject com.lighttable/csharp "0.0.3"
  :description "CSHARP language plugin for Light Table"
  :dependencies [[org.clojure/clojure "1.5.1"]])
 
